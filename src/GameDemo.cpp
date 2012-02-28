837d124a909e50e64664b51657b41c2581a197b4
黄程 <sniperbat@gmail.com>
2012-02-28 17:53:13 +0800

    测试用例更新 工程文件更新。

--- src/GameDemo.cpp
+++ src/GameDemo.cpp
@@ -2,22 +2,31 @@
 #include "ChsEntity.h"
 #include "ChsResourceManager.h"
 #include "ChsRenderSystem.h"
+#include "geometry/ChsPlane.h"
+#include "ChsMaterial.h"
+#include "ChsShaderProgram.h"
+
 //--------------------------------------------------------------------------------------------------
 using namespace Chaos;
 
 GameDemo game;
-
-ChsEntity * cube;
-
+static float transY = 0.0f;
 //--------------------------------------------------------------------------------------------------
 void GameDemo::onInit( void ) {
 	ChsEntityManager * resMgr = ChsResourceManager::sharedEntityManager();
-	cube = resMgr->getEntity( "cube" );
-	this->render()->root()->add( "cube", cube );
+	ChsEntity * plane = resMgr->getEntity( "planeEntity" );
+	ChsPlane * planeMesh = new ChsPlane(1.0f,1.0f);
+	planeMesh->setMaterial();
+	ChsMaterial * material = planeMesh->getMaterial();
+	material->connectUniform("translate", (void *)&transY, CHS_UNIFORM_1_FLOAT,1);
+	material->hasVertexColor(true);
+	plane->add("planeMesh", planeMesh);
+	this->render()->root()->add( "planeEntity", plane );
 }
 
 //--------------------------------------------------------------------------------------------------
 void GameDemo::onUpdate( void ) {
+	transY += 0.075f;
 }
 
 //--------------------------------------------------------------------------------------------------
