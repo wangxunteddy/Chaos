30be8496d1a5a3d9bc3dc621b6a0b93d8814b9b5
黄程 <sniperbat@gmail.com>
2012-03-02 15:55:36 +0800

    暂时去掉节点上的matrix 还没有决定如何设置matrix 是关联到node呢，还是到rendernode，还是到entity

--- src/chaos/ChsRenderNode.cpp
+++ src/chaos/ChsRenderNode.cpp
@@ -3,21 +3,20 @@
 #include "math/ChsMatrix.h"
 
 namespace Chaos {
-	//----------------------------------------------------------------------------------------------
-
 
 	//----------------------------------------------------------------------------------------------
 	ChsRenderNode::ChsRenderNode( std::string name ) : ChsNode( name ) {
-//		this->_worldMatrix = new ChsMatrix();
+		this->visible( true );
 	}
 
 	//----------------------------------------------------------------------------------------------
 	ChsRenderNode::~ChsRenderNode( void ){
-//		safeDelete( &this->_worldMatrix ); 
 	}
 
 	//----------------------------------------------------------------------------------------------
 	void ChsRenderNode::render( ChsRenderSystem * render ){
+		if(!this->visible())
+			return;//if this node is invisible,then all chilren are invisible too
 		this->doRender( render );
 		ChsNodeList::iterator iter =  this->children.begin();
 		ChsNodeList::iterator endIter =  this->children.end();
