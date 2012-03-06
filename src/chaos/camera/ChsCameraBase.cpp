ce2c5b4769d28e8f6db261b207f270ba850f8c0b
黄程 <sniperbat@gmail.com>
2012-03-06 13:25:26 +0800

    添加camera基础类。 目前可以直接使用此类生成一个基本camera 计划以后继承出各种特定的camera， 比如第一人称camera，上帝模式camera等。

---
+++ src/chaos/camera/ChsCameraBase.cpp
@@ -0,0 +1,71 @@
+﻿#include "ChsCameraBase.h"
+
+//--------------------------------------------------------------------------------------------------
+namespace Chaos {
+	
+	//----------------------------------------------------------------------------------------------
+	ChsCameraBase::ChsCameraBase( void ) : up( 0.0f, 1.0f, 0.0f ) {
+		this->mtxView.identity();
+		this->mtxProjection.identity();
+		this->isNeedUpdateViewMatrix = false;
+	}
+
+	//----------------------------------------------------------------------------------------------
+	ChsCameraBase::~ChsCameraBase( void ){
+	}
+
+	//----------------------------------------------------------------------------------------------
+	void ChsCameraBase::update( void ){
+		if( this->isNeedUpdateViewMatrix ){
+			this->isNeedUpdateViewMatrix = false;
+			this->mtxView.lookAt( this->position , this->target , this->up );
+		}
+		this->mtxViewProj = this->mtxView * this->mtxProjection;
+	}
+	
+	//----------------------------------------------------------------------------------------------
+	void ChsCameraBase::lookAt(const ChsVector3& position , const ChsVector3& target){
+		this->moveTo( position );
+		this->targetTo( target );
+	}
+
+	//----------------------------------------------------------------------------------------------
+	void ChsCameraBase::lookAt( float posX, float posY, float posZ, float targetX, float targetY, float targetZ ){
+		this->moveTo( posX, posY, posZ );
+		this->targetTo( targetX, targetY, targetZ );
+	}
+
+	//----------------------------------------------------------------------------------------------
+	void ChsCameraBase::perspective(float fovy, float aspect, float near, float far ){
+		this->prjFovy = fovy;
+		this->prjAspect = aspect;
+		this->prjNear = near;
+		this->prjFar = far;
+		this->mtxProjection.perspective( fovy, aspect, near, far );
+	}
+
+	//----------------------------------------------------------------------------------------------
+	void ChsCameraBase::moveTo( float x, float y, float z ){
+		this->position.x = x;
+		this->position.y = y;
+		this->position.z = z;
+		this->isNeedUpdateViewMatrix = true;
+	}
+	//----------------------------------------------------------------------------------------------
+	void ChsCameraBase::moveTo( const ChsVector3 & position ){
+		this->position = position;
+		this->isNeedUpdateViewMatrix = true;
+	}
+	//----------------------------------------------------------------------------------------------
+	void ChsCameraBase::targetTo( float x, float y, float z ){
+		this->target.x = x;
+		this->target.y = y;
+		this->target.z = z;
+		this->isNeedUpdateViewMatrix = true;
+	}
+	//----------------------------------------------------------------------------------------------
+	void ChsCameraBase::targetTo( const ChsVector3 & target ){
+		this->target = target;
+		this->isNeedUpdateViewMatrix = true;
+	}
+}
---
+++ src/chaos/camera/ChsCameraBase.h
@@ -0,0 +1,53 @@
+﻿#ifndef _CHS_CAMERABASE_H
+#define _CHS_CAMERABASE_H
+#pragma once
+
+#include "math/ChsVector3.h"
+#include "math/ChsMatrix.h"
+
+//--------------------------------------------------------------------------------------------------
+namespace Chaos {
+
+	//----------------------------------------------------------------------------------------------
+	class ChsCameraBase {
+	public:
+		ChsCameraBase( void );
+		virtual ~ChsCameraBase( void );
+
+		virtual	void update( void );
+		
+		virtual	void lookAt( const ChsVector3& position , const ChsVector3& target );
+		virtual	void lookAt( float posX, float posY, float posZ, float targetX, float targetY, float targetZ );
+		virtual	void perspective( float fovy , float aspect , float near , float far );
+
+		void moveTo( float x, float y, float z );
+		void moveTo( const ChsVector3 & position );
+		
+		void targetTo( float x, float y, float z );
+		void targetTo( const ChsVector3 & target );
+
+		inline ChsMatrix & getViewProjectionMatrix( void );
+	protected:
+		bool isNeedUpdateViewMatrix;
+		
+		ChsMatrix mtxView;
+		ChsMatrix mtxProjection;
+		ChsMatrix mtxViewProj;
+		
+		ChsVector3 position;
+		ChsVector3 target;
+		ChsVector3 up;
+		
+		float prjFovy;
+		float prjAspect;
+		float prjNear;
+		float prjFar;
+
+	};
+
+	//----------------------------------------------------------------------------------------------
+	inline ChsMatrix & ChsCameraBase::getViewProjectionMatrix( void ){
+		return this->mtxViewProj;
+	}
+}
+#endif	//_CHS_CAMERABASE_H
