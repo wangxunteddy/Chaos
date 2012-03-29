faa3a6aaa9c1e118f8ff73bc100e2939db2ba63a
黄程 <sniperbat@gmail.com>
2012-03-29 14:43:12 +0800

    分离Dae结构到专门的头文件，以便以后复用 初始化mesh的时候添加mesh的名字

--- src/chaos/ChsDaeLoader.cpp
+++ src/chaos/ChsDaeLoader.cpp
@@ -1,14 +1,8 @@
-#define BOOST_NO_CHAR16_T
-#define BOOST_NO_CHAR32_T
-#include <boost/lexical_cast.hpp>
-#include <boost/algorithm/string.hpp>
-#include <boost/foreach.hpp>
 #include <boost/assign.hpp>
 using namespace boost::assign;
-#include <boost/algorithm/string/split.hpp>
 #include <boost/scoped_array.hpp>
 
-
+#include "ChsDae.h"
 #include "ChsDaeLoader.h"
 #include "ChsModel.h"
 #include "ChsMesh.h"
@@ -19,169 +13,7 @@ using namespace boost::assign;
 #include "ChsIndexBuffer.h"
 #include "ChsVertexBuffer.h"
 
-
 namespace Chaos {
-	//----------------------------------------------------------------------------------------------
-	template<typename T> void lexicalCastToArray( std::vector<T> & array, std::string stream );
-	template<typename DaeInputType> void prepareInput( std::vector<DaeInputType> & inputs, tinyxml2::XMLElement * inputElement );
-	//----------------------------------------------------------------------------------------------
-	struct DaeParam{
-		std::string name;
-		std::string type;
-	};
-	
-	//----------------------------------------------------------------------------------------------
-	struct DaeAccessor{
-		std::string source;
-		int count;
-		int stride;
-		std::vector<DaeParam> param;
-		~DaeAccessor( void ){
-			this->param.clear();
-		}
-		void setValue( tinyxml2::XMLElement * accessorElement ){
-			this->source = accessorElement->Attribute( "source" );
-			this->count = accessorElement->IntAttribute( "count" );
-			this->stride = accessorElement->IntAttribute( "stride" );
-		}
-	};
-	
-	//----------------------------------------------------------------------------------------------
-	struct DaeTechniqueCommon{
-		DaeAccessor accessor;
-	};
-
-	//----------------------------------------------------------------------------------------------
-	struct DaeFloatArray{
-		std::string id;
-		int count;
-		std::vector<float> data;
-		~DaeFloatArray( void ){
-			this->data.clear();
-		}
-		void setValue( tinyxml2::XMLElement * arrayElement ){
-			this->id = arrayElement->Attribute( "id" );
-			this->count = arrayElement->IntAttribute( "count" );
-			lexicalCastToArray( this->data, arrayElement->GetText() );
-		}
-	};
-	
-	//----------------------------------------------------------------------------------------------
-	struct DaeSource{
-		std::string id;
-		DaeFloatArray floatArray;
-		DaeTechniqueCommon techniqueCommon;
-		void setValue( tinyxml2::XMLElement * sourceElement ){
-			this->id = sourceElement->Attribute( "id" );
-			this->floatArray.setValue( sourceElement->FirstChildElement( "float_array" ) );
-			tinyxml2::XMLElement * accessorElement = sourceElement->FirstChildElement( "technique_common" )->FirstChildElement( "accessor" );
-			this->techniqueCommon.accessor.setValue( accessorElement );
-		}
-	};
-	
-	//----------------------------------------------------------------------------------------------
-	typedef struct DaeInput{
-		std::string semantic;
-		std::string source;
-		void setValue( tinyxml2::XMLElement * inputElement ){
-			this->semantic = inputElement->Attribute( "semantic" );
-			//get source with strip the first char "#"
-			this->source = inputElement->Attribute( "source" ) + 1;
-		}
-	} DaeUnsharedInput;
-
-	//----------------------------------------------------------------------------------------------
-	struct DaeSharedInput : DaeInput{
-		int offset;
-		int set;
-		void setValue( tinyxml2::XMLElement * inputElement ){
-			DaeUnsharedInput::setValue( inputElement );
-			this->offset = inputElement->IntAttribute( "offset" );
-			this->set = -1;
-			inputElement->QueryIntAttribute( "set", &(this->set) );
-		}
-	};
-	
-	//----------------------------------------------------------------------------------------------
-	struct DaeVertices{
-		std::string id;
-		std::vector<DaeUnsharedInput> input;
-		~DaeVertices( void ){
-			this->input.clear();
-		}
-		void setValue( tinyxml2::XMLElement * verticesElement ){
-			this->id = verticesElement->Attribute( "id" );
-			prepareInput<DaeUnsharedInput>( this->input, verticesElement->FirstChildElement( "input" ) );
-		}
-	};
-	
-	//----------------------------------------------------------------------------------------------
-	struct DaeTriangles{
-		std::string material;
-		int count;
-		std::vector<DaeSharedInput> input;
-		std::vector<int> p;
-		~DaeTriangles( void ){
-			this->input.clear();
-			this->p.clear();
-		}
-		void setValue( tinyxml2::XMLElement * triangleElement ){
-			this->material = triangleElement->Attribute( "material" );
-			this->count = triangleElement->IntAttribute( "count" );
-			prepareInput<DaeSharedInput>( this->input, triangleElement->FirstChildElement( "input" ) );
-			lexicalCastToArray( this->p, triangleElement->FirstChildElement( "p" )->GetText() );
-		}
-	};
-	
-	//----------------------------------------------------------------------------------------------
-	struct DaeMesh{
-		std::map<std::string,DaeSource> sources;
-		DaeVertices vertices;
-		DaeTriangles triangles;
-		~DaeMesh( void ){
-			this->sources.clear();
-		}
-		void setValue( tinyxml2::XMLElement * meshElement ){
-			tinyxml2::XMLElement * element = meshElement->FirstChildElement();
-			do{
-				std::string elementName = element->Name();
-				if( !elementName.compare( "source" ) ){
-					DaeSource source;
-					source.setValue( element );
-					insert( this-> sources ) ( source.id, source );
-				}
-				else if(!elementName.compare( "vertices" )){
-					this->vertices.setValue( element );
-				}
-				else if(!elementName.compare( "triangles" )){
-					this->triangles.setValue( element );
-				}
-				element = element->NextSiblingElement();
-			}while( element != NULL );
-		}
-	};
-
-	//----------------------------------------------------------------------------------------------
-	template<typename DaeInputType>
-	void prepareInput( std::vector<DaeInputType> & inputs, tinyxml2::XMLElement * inputElement ){
-		do{
-			DaeInputType input;
-			input.setValue( inputElement );
-			inputs += input;
-			inputElement = inputElement->NextSiblingElement( "input" );
-		}while( inputElement != NULL );
-	}
-
-	//----------------------------------------------------------------------------------------------
-	template<typename T>
-	void lexicalCastToArray( std::vector<T> & array, std::string stream ){
-		std::vector<std::string> rs;
-		boost::split( rs, stream, boost::is_any_of("\n ") );
-		BOOST_FOREACH( const std::string & str, rs)
-			if(!str.empty())
-				array += boost::lexical_cast<T>( str );
-	}
-
 	//----------------------------------------------------------------------------------------------
 	void reduceTriangleListAndVertexIndexList( DaeMesh & daeMesh, std::vector<int> & vertexIndexList, std::vector<unsigned short> & triangleList );
 	void reduceTriangleListAndVertexIndexList( DaeMesh & daeMesh, std::vector<int> & vertexIndexList, std::vector<unsigned short> & triangleList ){
@@ -311,6 +143,7 @@ namespace Chaos {
 		tinyxml2::XMLElement * meshElement;
 		do{
 			meshElement = geometryElement->FirstChildElement( "mesh" );
+			std::string meshName = geometryElement->Attribute( "id" );
 			DaeMesh daeMesh;
 			daeMesh.setValue( meshElement );
 			reduceTriangleListAndVertexIndexList( daeMesh, vertexIndexList, triangleList );
@@ -319,7 +152,7 @@ namespace Chaos {
 			makeVertexAttributes( daeMesh, vertexAttributes );
 			makeVertexList( vertexIndexList, vertexList, vertexAttributeCount, vertexAttributes );
 			vertexIndexList.clear();
-			boost::shared_ptr<ChsMesh> mesh( new ChsMesh() );
+			boost::shared_ptr<ChsMesh> mesh( new ChsMesh( meshName ) );
 			for( int i = 0; i < vertexAttributeCount ; i++ ){
 				const VertexAttribute & attribute = vertexAttributes[i];
 				bool isNormalized = attribute.name.compare( "normal" ) ? false : true;
