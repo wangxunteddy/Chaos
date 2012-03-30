b52984d7c7eac519e7641c9b1a75e1fc39613a52
黄程 <sniperbat@gmail.com>
2012-03-30 09:44:24 +0800

    实现对xml格式chsmodelx的支持

--- src/chaos/ChsModelLoader.cpp
+++ src/chaos/ChsModelLoader.cpp
@@ -5,20 +5,12 @@
 #include "ChsIndexBuffer.h"
 #include "ChsVertexBuffer.h"
 #include "io/ChsFileSystem.h"
-
+#include "ChsUtility.h"
 //--------------------------------------------------------------------------------------------------
 namespace Chaos {
-
-	//----------------------------------------------------------------------------------------------
-	ChsModel * ChsModelLoader::loadAsXML( const char *filename ){
-		char * data;
-		ChsFileSystem::sharedInstance()->readFileAsUTF8( filename, &data );
-		boost::scoped_ptr<char> modelData( data );
-		
-	}
-	
 	//----------------------------------------------------------------------------------------------
 	#define SKIP( data, size ) (data) += size
+	
 	//----------------------------------------------------------------------------------------------
 	template<typename T>
 	T readData( char ** data ){
@@ -26,6 +18,7 @@ namespace Chaos {
 		SKIP( *data, sizeof(T) );
 		return value;
 	}
+	
 	//----------------------------------------------------------------------------------------------
 	std::string readString( char ** data );
 	std::string readString( char ** data ){
@@ -37,6 +30,63 @@ namespace Chaos {
 		SKIP( *data, strCount );
 		return str;
 	}
+	
+	//----------------------------------------------------------------------------------------------
+	ChsModel * ChsModelLoader::loadAsXML( const char *filename ){
+		char * data;
+		ChsFileSystem::sharedInstance()->readFileAsUTF8( filename, &data );
+		boost::scoped_ptr<char> modelData( data );
+		tinyxml2::XMLDocument doc;
+		int ret = doc.Parse( data );
+		if( tinyxml2::XML_NO_ERROR != ret ){
+			printf( "errorStr1:%s\n", doc.GetErrorStr1() );
+			printf( "errorStr2:%s\n", doc.GetErrorStr2() );
+			doc.PrintError();//get some error
+			return NULL;
+		}
+		
+		ChsModel * model = NULL;
+		tinyxml2::XMLElement * modelElement = doc.FirstChildElement( "ChsModel" );
+		if( !modelElement ){
+			printf( "there no model elements\n" );
+			return NULL;
+		}
+		
+		std::string modelName = modelElement->Attribute( "name" );
+		model = new ChsModel( modelName );
+		tinyxml2::XMLElement * meshElement = modelElement->FirstChildElement( "ChsMesh" );
+		while( meshElement ){
+			std::string meshName = meshElement->Attribute( "name" );
+			boost::shared_ptr<ChsMesh> mesh( new ChsMesh( meshName ) );
+			
+			tinyxml2::XMLElement * attrElement = meshElement->FirstChildElement( "ChsAttribute" );
+			while ( attrElement ) {
+				std::string attrName = attrElement->Attribute( "name" );
+				int stride = attrElement->IntAttribute( "stride" );
+				bool isNormalized = false;
+				if( !attrName.compare( "normal" ) )
+					isNormalized = true;
+				mesh->vertexBuffer->addAttrib( stride, GL_FLOAT, isNormalized, attrName );
+				attrElement = attrElement->NextSiblingElement( "ChsAttribute" );
+			}
+			
+			std::vector<float> vertices;
+			lexicalCastToArray( vertices, meshElement->FirstChildElement( "ChsVertexBuffer" )->GetText() );
+			mesh->vertexBuffer->setData( vertices );
+			vertices.clear();
+			std::vector<unsigned short> indeices;
+			lexicalCastToArray( indeices, meshElement->FirstChildElement( "ChsIndexBuffer" )->GetText() );
+			mesh->indexBuffer->setData( indeices );
+			indeices.clear();
+			mesh->indexBuffer->mode( GL_TRIANGLES );
+			mesh->setMaterial();
+			model->addMesh( mesh );
+			meshElement = meshElement->NextSiblingElement();
+		}
+		
+		return model;
+	}
+	
 	//----------------------------------------------------------------------------------------------
 	ChsModel * ChsModelLoader::loadAsBinary( const char *filename ){
 		char * data;
@@ -51,8 +101,8 @@ namespace Chaos {
 		ChsModel * model = new ChsModel( modelName );
 		int meshCount = readData<int>( &data );
 		for( int meshIdx = 0; meshIdx < meshCount; meshIdx++ ){
-			boost::shared_ptr<ChsMesh> mesh( new ChsMesh() );
 			std::string meshName = readString( &data );
+			boost::shared_ptr<ChsMesh> mesh( new ChsMesh( meshName ) );
 			int attriCount = readData<int>( &data );
 			for( int attrIdx = 0; attrIdx < attriCount; attrIdx++ ){
 				std::string attrName = readString( &data );
@@ -75,12 +125,12 @@ namespace Chaos {
 		return model;
 	}
 	
+	//----------------------------------------------------------------------------------------------
 	ChsModel * ChsModelLoader::load( const char *filename ){
 		std::string fileName = filename;
-		//if( fileName.find_last_of( ".chsmodelx" ) != std::string::npos )
-		//	return this->loadAsXML( filename );
-		//else 
+		if( fileName.find( ".chsmodelx" ) != std::string::npos )
+			return this->loadAsXML( filename );
+		else 
 			return this->loadAsBinary( filename );
-		
 	}
 }
